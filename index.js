cf786a1c5866c55b9452ca03a112536b16ab82bc
testmj <testmjmj192@gmail.com>
2025-09-10 12:39:07 +0530

    code added

--- index.js
+++ index.js
@@ -10,87 +10,102 @@ const io = new Server(server);
 
 app.use(express.static(path.join(__dirname, "public")));
 
-// separate queues
 let waitingVideo = null;
 let waitingText = null;
-
-function pair(a, b, mode) {
-  const room = a.id + "#" + b.id;
-  a.join(room);
-  b.join(room);
-  a.emit("paired", { partnerId: b.id, mode, room });
-  b.emit("paired", { partnerId: a.id, mode, room });
-  console.log(`Paired (${mode}): ${a.id} <-> ${b.id} in room ${room}`);
-}
 
 io.on("connection", (socket) => {
   console.log("connected:", socket.id);
 
-  // join queue
+  // --- JOIN ---
   socket.on("join", (mode) => {
-    socket.mode = mode;
+    if (mode === "video") {
+      if (!waitingVideo) {
+        waitingVideo = socket;
+        socket.mode = "video";
+        socket.emit("waiting");
+      } else if (waitingVideo.id !== socket.id) {
+        const partner = waitingVideo;
+        waitingVideo = null;
+        const room = socket.id + "#" + partner.id;
+        socket.join(room);
+        partner.join(room);
+        socket.emit("paired", { partnerId: partner.id, mode, room });
+        partner.emit("paired", { partnerId: socket.id, mode, room });
+      }
+    } else if (mode === "text") {
+      if (!waitingText) {
+        waitingText = socket;
+        socket.mode = "text";
+        socket.emit("waiting");
+      } else if (waitingText.id !== socket.id) {
+        const partner = waitingText;
+        waitingText = null;
+        const room = socket.id + "#" + partner.id;
+        socket.join(room);
+        partner.join(room);
+        socket.emit("paired", { partnerId: partner.id, mode, room });
+        partner.emit("paired", { partnerId: socket.id, mode, room });
+      }
+    }
+  });
+
+  // --- NEXT ---
+  socket.on("next", () => {
+    const mode = socket.mode;
+    if (!mode) return;
+
+    const rooms = [...socket.rooms].filter((r) => r !== socket.id);
+    rooms.forEach((r) => socket.leave(r));
+    io.to(rooms).emit("partner-left");
 
     if (mode === "video") {
       if (!waitingVideo) {
         waitingVideo = socket;
         socket.emit("waiting");
       } else if (waitingVideo.id !== socket.id) {
-        pair(waitingVideo, socket, "video");
+        const partner = waitingVideo;
         waitingVideo = null;
+        const room = socket.id + "#" + partner.id;
+        socket.join(room);
+        partner.join(room);
+        socket.emit("paired", { partnerId: partner.id, mode, room });
+        partner.emit("paired", { partnerId: socket.id, mode, room });
       }
     } else if (mode === "text") {
       if (!waitingText) {
         waitingText = socket;
         socket.emit("waiting");
       } else if (waitingText.id !== socket.id) {
-        pair(waitingText, socket, "text");
+        const partner = waitingText;
         waitingText = null;
+        const room = socket.id + "#" + partner.id;
+        socket.join(room);
+        partner.join(room);
+        socket.emit("paired", { partnerId: partner.id, mode, room });
+        partner.emit("paired", { partnerId: socket.id, mode, room });
       }
     }
   });
 
-  // relay WebRTC signals
+  // --- CHAT ---
+  socket.on("chat", ({ room, message }) => {
+    io.to(room).emit("chat", { from: socket.id, message });
+  });
+
+  // --- SIGNAL (WebRTC) ---
   socket.on("signal", ({ to, data }) => {
     io.to(to).emit("signal", { from: socket.id, data });
   });
 
-  // relay text chat
-  socket.on("chat", ({ room, message }) => {
-    socket.to(room).emit("chat", { from: socket.id, message });
-  });
-
-  // next
-  socket.on("next", () => {
-    const rooms = Array.from(socket.rooms).filter((r) => r !== socket.id);
-    rooms.forEach((r) => socket.to(r).emit("partner-left"));
-    rooms.forEach((r) => socket.leave(r));
-
-    if (socket.mode === "video") {
-      if (!waitingVideo) {
-        waitingVideo = socket;
-        socket.emit("waiting");
-      } else if (waitingVideo.id !== socket.id) {
-        pair(waitingVideo, socket, "video");
-        waitingVideo = null;
-      }
-    } else if (socket.mode === "text") {
-      if (!waitingText) {
-        waitingText = socket;
-        socket.emit("waiting");
-      } else if (waitingText.id !== socket.id) {
-        pair(waitingText, socket, "text");
-        waitingText = null;
-      }
-    }
-  });
-
+  // --- DISCONNECT ---
   socket.on("disconnect", () => {
-    console.log("disconnected:", socket.id);
     if (waitingVideo && waitingVideo.id === socket.id) waitingVideo = null;
     if (waitingText && waitingText.id === socket.id) waitingText = null;
 
-    const rooms = Array.from(socket.rooms).filter((r) => r !== socket.id);
-    rooms.forEach((r) => socket.to(r).emit("partner-left"));
+    const rooms = [...socket.rooms].filter((r) => r !== socket.id);
+    rooms.forEach((r) => {
+      io.to(r).emit("partner-left");
+    });
   });
 });
 
